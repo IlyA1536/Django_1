1c60d0d10ccd9205de1638d1feb70dafe8d3b466
IlyA1536 <i0977187143@gmailcom>
2024-02-03 13:01:06 +0200

    Did a database with 2 tables: User, Task

---
+++ django_orm/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class User(models.Model):
+    login = models.CharField(max_length=63)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=50, choices=[('AD', 'admin'), ('US', 'user')])
+    task = models.ForeignKey("Task", on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.login
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.TextField()
+    time_of_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
+    
---
+++ main.py
@@ -0,0 +1,4 @@
+import django_setup
+
+from django_orm.models import User, Task
+
